bd3fce297fd782b2aa6499f2a5cee3fbd80d703d
chris <cseiter@gmail.com>
2021-07-05 19:47:42 -0400

    calculate integer for square root

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+
+def prime?(int)
+    #0 and 1 are not prime
+    return false if int <= 1
+    #prime numbers are numbers whose factors
+    #are only 1 and itself searching through
+    #divisors only takes from 1 to the square root
+    #since the factors only "pair" up to that point 
+    puts (Math.sqrt(int)).to_i
+end
+
+prime?(42)
