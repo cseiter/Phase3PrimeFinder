ebc811ff6b2024f1bf16daf38352740c5151df4b
chris <cseiter@gmail.com>
2021-07-05 19:56:25 -0400

    create modulo array

--- prime.rb
+++ prime.rb
@@ -1,6 +1,7 @@
 # Add  code here!
 
 def prime?(int)
+    modulo_array = []
     #0 and 1 are not prime
     return false if int <= 1
     #prime numbers are numbers whose factors
@@ -8,7 +9,8 @@ def prime?(int)
     #divisors only takes from 1 to the square root
     #since the factors only "pair" up to that point 
     max_number = (Math.sqrt(int)).to_i
-    (2..max_number).each {|n| puts (int % n)}
+    (2..max_number).each {|n| modulo_array << (int % n)}
+    puts modulo_array
 end
 
 prime?(42)
